7a1974f7daba8c4f7f584942233ee330635883d7
Efosee <valeraparaskev@gmail.com>
2024-04-18 16:06:04 +0300

    project v1.0

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import asyncio
+from aiogram import Bot, Dispatcher, F
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, StateFilter
+from aiogram.client.default import DefaultBotProperties
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from core.redis.storage import storage
+from core.utils.config import config
+from core.handlers.users import start
+from core.handlers.admins import ban, unban, delete
+from core.handlers.calculation.enter_vuz import start_calculation, entering_fc_vuz, entering_next_vuz, callback_processing, callback_cancel
+from core.handlers.calculation.enter_criteria import callback_processing_criteria, entering_fc_criteria, entering_next_criteria, callback_cancel_criteria
+from core.handlers.calculation.calculation import record_compare, record_compare_vuz
+from core.middlewaries.isbanned import IsBanned
+from core.filters.isadmin import IsAdminFilter
+from core.keyboards.inline import AddingList, Compare
+bot = Bot(token=config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
+dp = Dispatcher(storage=storage)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def main():
+    dp.update.middleware.register(IsBanned())
+    dp.message.register(start, Command("start"))
+    dp.message.register(ban, Command("ban"), IsAdminFilter())
+    dp.message.register(unban, Command("unban"), IsAdminFilter())
+    dp.message.register(delete, Command("del"), IsAdminFilter())
 
+    dp.message.register(start_calculation, Command("calc"))
+    dp.message.register(entering_fc_vuz, StateFilter("entering_fc_vuz"))
+    dp.callback_query.register(callback_processing, StateFilter("entering_next_vuz"), AddingList.filter(F.action.in_(["add", "next"])))
+    dp.callback_query.register(callback_cancel, StateFilter("entering_next_vuz"), AddingList.filter(F.action == "cancel"))
+    dp.message.register(entering_next_vuz, StateFilter("entering_next_vuz"))
 
-# Press the green button in the gutter to run the script.
+    dp.message.register(entering_fc_criteria, StateFilter("entering_fc_criteria"))
+    dp.callback_query.register(callback_processing_criteria, StateFilter("entering_next_criteria"), AddingList.filter(F.action.in_(["add", "next"])))
+    dp.callback_query.register(callback_cancel_criteria, StateFilter("entering_next_criteria"), AddingList.filter(F.action == "cancel"))
+    dp.message.register(entering_next_criteria, StateFilter("entering_next_criteria"))
+
+    dp.callback_query.register(record_compare, StateFilter("compare_criteria"), Compare.filter())
+    dp.callback_query.register(record_compare_vuz, StateFilter("compare_vuz"), Compare.filter())
+    try:
+        await dp.start_polling(bot)
+    except KeyboardInterrupt:
+        await dp.stop_polling()
+    finally:
+        await dp.stop_polling()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
